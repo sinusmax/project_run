67e4ce6a90bc5f0a63dbe554939dcf5d4ecdf570
sinusmax <sinusmax@mail.ru>
2026-01-09 15:11:36 +0300

    create serializers.py, and ViewSet and router

--- app_run/views.py
+++ app_run/views.py
@@ -1,6 +1,11 @@
+from rest_framework import viewsets
 from rest_framework.decorators import api_view # чтобы использовать декоратор
 from rest_framework.response import Response # чтобы использовать Response от DRF
 from django.conf import settings # чтобы использовать переменные из settings
+
+from app_run.models import Run
+from app_run.serializers import RunSerializer
+
 
 # Create your views here.
 @api_view(['GET'])
@@ -17,3 +22,16 @@ def company_details_view(request):
         'contacts': settings.MY_COMPANY_ADDRESS,
     }
     return Response(details)
+
+
+class RunViewSet(viewsets.ModelViewSet):
+    """
+    Задание №2.
+    Отдаем по АПИ список забегов с комментариями.
+    Данные берем из БД.
+    Забег добавляется POST-запросом с указанием коммента и id-атлета
+    (запрос в данном случае идет на /api/runs)
+    """
+    queryset = Run.objects.all()
+    serializer_class = RunSerializer
+
