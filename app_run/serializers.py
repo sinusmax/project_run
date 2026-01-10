b0e31207c1512ebcd88072662df7929647afef4b
sinusmax <sinusmax@mail.ru>
2026-01-10 12:35:34 +0300

    Task №3. Added filtering feature by user type

--- app_run/serializers.py
+++ app_run/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Run
 
@@ -5,4 +6,18 @@ class RunSerializer(serializers.ModelSerializer):
     class Meta:
         model = Run
         fields = '__all__'
+
+
+class UserSerializer(serializers.ModelSerializer):
+    type = serializers.SerializerMethodField()
+
+    class Meta:
+        model = User
+        fields = ['id', 'date_joined', 'username', 'last_name', 'first_name', 'type']
+
+    def get_type(self, obj):
+        if obj.is_staff or obj.is_superuser:
+            return 'coach'
+        else:
+            return 'athlete'
 
--- app_run/views.py
+++ app_run/views.py
@@ -1,10 +1,11 @@
+from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.decorators import api_view # чтобы использовать декоратор
 from rest_framework.response import Response # чтобы использовать Response от DRF
 from django.conf import settings # чтобы использовать переменные из settings
 
 from app_run.models import Run
-from app_run.serializers import RunSerializer
+from app_run.serializers import RunSerializer, UserSerializer
 
 
 # Create your views here.
@@ -34,4 +35,25 @@ class RunViewSet(viewsets.ModelViewSet):
     """
     queryset = Run.objects.all()
     serializer_class = RunSerializer
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    Задание №3.
+    Отдаем по АПИ список атлетов и тренеров с фильтрацией по типу пользователя.
+    Суперадмина не отдаем никогда.
+    Фильтр передается в GET-запросе в параметре ?type=coach|athlete
+    (запрос в данном случае идет на /api/users)
+    """
+    queryset = User.objects.filter(is_superuser = False) # сразу исключаем суперадминов
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        qs = self.queryset # наверное, можно и не вводить лишнюю переменную qs, а использовать прям queryset
+        type_user = self.request.query_params.get('type', None) # .query_params. вместо .GET.
+        if type_user == 'coach':
+            qs = qs.filter(is_staff = True)
+        if type_user == 'athlete':
+            qs = qs.filter(is_staff = False)
+        return qs
 
