ec4f3102974483cbaba7e1a9a69e3adffd4f2753
sinusmax <sinusmax@mail.ru>
2026-01-10 15:51:36 +0300

    added settings for django-debug-toolbar

--- project_run/settings/local.py
+++ project_run/settings/local.py
@@ -9,3 +9,12 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# добавил для django debug toolbar
+INSTALLED_APPS.append('debug_toolbar')
+MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+    # '0.0.0.0', # Если нужно для других локальных сетей
+]
--- project_run/urls.py
+++ project_run/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
@@ -30,4 +31,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/company_details/', company_details_view),
     path('', include(router.urls)),
-]
+] + debug_toolbar_urls()
+
