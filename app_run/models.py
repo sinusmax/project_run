2f667408ef715ca57d8585efe55d34210a5c3b8d
sinusmax <sinusmax@mail.ru>
2026-01-11 13:14:33 +0300

    Added choices-field 'status' in model Run

--- app_run/models.py
+++ app_run/models.py
@@ -6,3 +6,11 @@ class Run(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     comment = models.TextField()
     athlete = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # Задача №6. Добавляем в модель доп. поле 'status'
+    STATUS_CHOICES = {
+        'init': 'Забег инициализирован',
+        'in_progress': 'Забег начат',
+        'finished': 'Забег закончен',
+    }
+    status = models.CharField(choices=STATUS_CHOICES, max_length=50, default='unknown')
